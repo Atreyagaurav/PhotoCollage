80c8e9ae866ba1cc3638bb8b5a873af96f8d9a36
Gaurav Atreya <atreygaurv98@gmail.com>
2019-09-05 13:24:36 +0545

    initial commit

--- PhotoCollage/PhotoCollage.py
+++ PhotoCollage/PhotoCollage.py
@@ -1,1 +1,17 @@
+import cv2
+def ResizeImage(img,res,str):
+	if(str=="Auto"):
+		height=int(1.1*res)
+		width=int(0.96*res)
+	elif(str=="Passport"):
+		height=int(1.3*res)
+		width=int(1.1*res)
+	elif(str=="MRP"):
+		height=int(45/25.4*res)
+		width=int(35/25.4*res)
+	dim=(height,width)
+	return cv2.resize(img,dim)
 
+img=cv2.imread("Pictures\\MyPic.jpg")
+resized=ResizeImage(img,150,"Auto")
+cv2.imwrite("Pictures\\MyPicRe.jpg",resized)
